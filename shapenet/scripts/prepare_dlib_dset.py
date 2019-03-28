f3b09558fdcb1654044b24afd56b03dda2095b75
tamvm <tamvm@vng.com.vn>
2019-03-28 16:47:55 +0700

    update script

--- shapenet/scripts/prepare_dlib_dset.py
+++ shapenet/scripts/prepare_dlib_dset.py
@@ -1,12 +1,68 @@
-import glob
 import os
-from shapedata.io import pts_importer
 from copy import deepcopy
-from shapedata import SingleShapeDataProcessing
 import numpy as np
 from sklearn.decomposition import PCA
+from skimage.transform import AffineTransform
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
 
-def lmk_pca(landmarks, scale = True, center = True):
+####################################################
+# image transformation method
+# Credit: https://github.com/justusschock/shapedata/
+####################################################
+
+def get_angle(v0, v1, v2, degree=False):
+
+    a1 = v0 - v1
+    a2 = v0 - v2
+    cosine_angle = np.dot(a1, a2) / (np.linalg.norm(a1) *
+                                     np.linalg.norm(a2))
+
+    angle = np.arccos(cosine_angle)
+
+    if degree:
+        angle = np.rad2deg(angle)
+
+    return angle
+
+
+def _normalize_rotation(lmks, img_size, index_left, index_right):
+
+    left = lmks[index_left]
+    right = lmks[index_right]
+
+    diff = left - right
+
+    middle = right + diff / 2
+
+    length_middle_left = np.sqrt(((left - middle) ** 2).sum())
+
+    left_optim = deepcopy(middle)
+    left_optim[-1] += length_middle_left
+    # print ('left', left, ' middle ', middle, ' left_optim ', left_optim)
+    # get rotation angle
+    rot_angle = get_angle(middle, left_optim, left, degree=False)
+
+    affine_trafo = AffineTransform(rotation=rot_angle)
+
+    shift_x, shift_y = np.array(img_size) / 2.
+
+    # transform to shift image to origin
+    tf_shift = AffineTransform(translation=[-shift_x, -shift_y])
+
+    # transform to shift image back to original position
+    tf_shift_inv = AffineTransform(translation=[shift_x, shift_y])
+
+    complete_trafo = AffineTransform((tf_shift + (affine_trafo + tf_shift_inv)).params)
+
+    r = complete_trafo(np.ascontiguousarray(lmks[:, [1, 0]]))[:, [1, 0]]
+    # print ('new left ', r[index_left], ' new right ', r[index_right])
+    return r
+    # return complete_trafo(np.ascontiguousarray(lmks)
+
+############################################################    
+
+def make_pca(landmarks, scale = True, center = True):
     """
     perform PCA on samples' landmarks
     Parameters
@@ -22,107 +78,96 @@ def lmk_pca(landmarks, scale = True, center = True):
         eigen_shapes
     """
     if center:
-        print ('  centering...')
         mean = np.mean(landmarks.reshape(-1, landmarks.shape[-1]), axis=0)
         landmarks = landmarks - mean
     landmarks_transposed = landmarks.transpose((0, 2, 1))
 
     reshaped = landmarks_transposed.reshape(landmarks.shape[0], -1)
 
-    print ('  pca fit...')
     pca = PCA()
     pca.fit(reshaped)
 
     if scale:
-        print ('  scaling...')
         components = pca.components_ * pca.singular_values_.reshape(-1, 1)
     else:
         components = pca.components_
-
 
     return np.array([pca.mean_] + list(components)).reshape(
         components.shape[0] + 1,
         *landmarks_transposed.shape[1:]).transpose(0, 2, 1)
 
-def list_files(directory):
-    files = []
-    extensions = [".pts"]    
-    for ext in extensions:
-        ext = ext.strip(".")
-        files += glob.glob(directory + "/*." + ext)        
-    files.sort()
-    # pt_files = [f.rsplit(".", 1)[0] + ".pts" for f in files]
-    # ne = [f for f in pt_files if not os.path.exists(f)]
-    # print (ne)
-    # assert len(ne) == 0
-    # return pt_files
-    return files 
+def view_landmarks(lmks, img = None, size = None):
+    """show landmark"""
+    # print(img)
+    marker_size = 15    
+    if img is not None and False:
+        plt.imshow(mpimg.imread(img))
+    else:
+        # convert to image origin
+        plt.gca().invert_yaxis()
+    plt.scatter(lmks[:, 0], lmks[:, 1], c="C0", s=marker_size)
 
-def load_pts(data_dir):
-    pt_files = list_files(data_dir)
-    return [pts_importer(f) for f in pt_files]
+    plt.show()
 
+def normalize_rotation(lmks, img_size, rotation_idxs=(37, 46)):    
+    for idx in range(len(lmks)):
+        # plot 
+        w, h, _ = img_size[idx]
+        lmks[idx] = _normalize_rotation(lmks[idx], (w, h), rotation_idxs[0], rotation_idxs[1])
+    return lmks
 
-def make_pca(data_dir, out_file, normalize_rot=False, rotation_idxs=()):
-    """
-    Creates a PCA from data in a given directory
+def load_landmarks(lmk_xml):
+    assert lmk_xml.endswith('.xml')
+    print('loading landmarks... ', lmk_xml)    
     
-    Parameters
-    ----------
-    data_dir : str
-        directory containing the image and landmark files
-    out_file : str
-        file the pca will be saved to
-    normalize_rot : bool, optional
-        whether or not to normalize the data's rotation
-    rotation_idxs : tuple, optional
-        indices for rotation normalization, msut be specified if 
-        ``normalize_rot=True``
-    
-    """
+    import xml.etree.ElementTree as ET
+    from PIL import Image 
+    tree = ET.parse(lmk_xml)
+    root = tree.getroot()
+    base = os.path.dirname(lmk_xml)
 
-    data_dir = os.path.abspath(data_dir)
-    out_file = os.path.abspath(out_file)
+    points = []
+    img_shapes = []
+    img_names = []
+    for img in root.iter('image'):
+        box = img.find('box')
+        path = os.path.join(base, img.get('file'))
+        xy = []
+        for p in box.iter('part'):
+            # xs.append(int(p.get('x')))
+            # ys.append(int(p.get('y')))
+            xy.append((int(p.get('x')), int(p.get('y'))))
+        # xs = np.array(xs, dtype=np.int32).reshape((-1, 1))
+        # ys = np.array(ys, dtype=np.int32).reshape((-1, 1))
+        # points.append(np.hstack([xs, ys]))
+        points.append(np.array(xy, dtype=np.int32))
+        img_names.append(img.get('file'))
+        bound = (int(box.get('top')), int(box.get('left')), int(box.get('width')), int(box.get('height')))
+        with Image.open(path) as imgRef:
+            w, h = imgRef.size
+            img_shapes.append((w, h, bound))
 
-    lmks = load_pts(data_dir)
-    print ('no imgs with points = ', len(lmks))
-    assert lmks is not None
-    # make use of existing class
-    # data = [SingleShapeDataProcessing(None, d) for d in lmks]
+    return np.array(points), img_shapes, img_names
 
-    if normalize_rot:
-        print('normalize rotation...')
-        for idx in range(len(lmks)):
-            processor = SingleShapeDataProcessing(None, lmks[idx])
-            lmks[idx] = processor.normalize_rotation(rotation_idxs[0], rotation_idxs[1])
-
-    print('calculate pca...')
-    pca = lmk_pca(np.array(lmks))
-    # print (pca)
-    assert out_file.endswith('.npz')
-    np.savez(out_file, shapes=pca)
-
-def prepare_dlib_dset(data_dir):
+def prepare_dlib_dset(lmk_xml):
     """
     Prepare datasets similar to one available in dlib.
     Download according to this instruction 
     https://medium.com/datadriveninvestor/training-alternative-dlib-shape-predictor-models-using-python-d1d8f8bd9f5c
     """
-    print('Prepare dlib dataset ', data_dir)    
-    dsets = ['afw', 'helen', 'ibug', 'lfpw']
-    # dsets = ['helen', 'ibug', 'lfpw']
-    for ds in dsets:
-        p = os.path.join(data_dir, ds)
-        train_dir = os.path.join(p, 'trainset')
-        train_dir = train_dir if os.path.exists(train_dir) else p
-        output = os.path.join(data_dir, ds + ".train_pca.npz")
-        if not os.path.exists(output):
-            print ('make pca for ', ds)
-            make_pca(train_dir,
-                  output, 
-                  rotation_idxs=(37, 46))
-        else:
-            print (ds, ' is already preprocessed') 
+    base_dir = os.path.dirname(lmk_xml)
+    pca_file = os.path.join(base_dir, 'train_pca.npz')
+    if not os.path.exists(pca_file):
+        points, img_sizes, imgs = load_landmarks(lmk_xml)
+        # view_landmarks(points[20], os.path.join(base_dir, imgs[20]), img_sizes[20])
+        points = normalize_rotation(points, img_sizes)
+        # view_landmarks(points[20])
+        pca = make_pca(points, img_sizes)        
+        print('pca size = ', pca[0].shape)
+        print(pca[0])
+        # np.savez(pca_file, shapes=pca)
+    else:
+        print ('pca file already exists at ', pca_file)
 
 if __name__ == '__main__':
     import argparse
@@ -132,4 +177,5 @@ if __name__ == '__main__':
                         type=str)
     args = parser.parse_args()
     data_dir = args.dataset
-    prepare_dlib_dset(data_dir if data_dir is not None else '/home/tamvm/Downloads/ibug_300W_large_face_landmark_dataset')
+    prepare_dlib_dset(data_dir if data_dir is not None 
+        else '/home/tamvm/Downloads/ibug_300W_large_face_landmark_dataset/labels_ibug_300W_train.xml')
